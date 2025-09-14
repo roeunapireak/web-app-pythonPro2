2dcaffb5cef1b4787ec1bc9f01fcaf91b18e9a13
RoeunApireak <apireak.roeun.datau@gmail.com>
2025-09-14 14:43:35 +0700

    commit

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import (Flask, session, request, 
                    redirect, url_for)
 
+from db_script import get_question_after, get_quises, main 
+
+main()
+
+def start_quis(quiz_id):
+    '''creates the desired values in the session dictionary'''
+    session['quiz'] = quiz_id
+    session['last_question'] = 0
 
 
 
 def index():
-    return "hello world"
+    '''First page: if it came with a GET request, then choose a quiz, 
+    if POST, then remember the quiz ID and send it to the questions'''
+
+    if request.method == 'GET':
+        start_quis(-1)
+        return quiz_form()
 
 
 
